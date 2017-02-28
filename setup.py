f2e6c093f427f6a2b005bc3d3d63913a0604ae8c
hanfang <hanfang.cshl@gmail.com>
2017-02-28 18:12:58 -0500

    [update]

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 import os
 from setuptools import setup, find_packages
-from codecs import open
-
-#from os import path
-#here = path.abspath(path.dirname(__file__))
 
 cmd = 'gfortran ./glmnet_python/GLMnet.f -fPIC -fdefault-real-8 -shared -o ./glmnet_python/GLMnet.so'
 os.system(cmd)
 
-setup(name='glmnet_python_hanfang',
+setup(name='glmnet_python_hf',
 	version = '0.1',
 	description = 'Python version of glmnet, originally from Stanford University, modified by Han Fang',
         url="https://github.com/hanfang/glmnet_python",
@@ -17,4 +13,3 @@ setup(name='glmnet_python_hanfang',
 	license = 'GPL-2',
 	packages=['glmnet_python'],
 	zip_safe = False)
-
