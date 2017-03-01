26d4ae4233d3d3bc02ab2d8532d85d8db9720c5b
hanfang <hanfang.cshl@gmail.com>
2017-03-01 17:04:03 -0500

    [fix] setup file for fortran code

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-import os
-from setuptools import setup, find_packages
+import os, sys
+# from setuptools import setup, find_packages
+from numpy.distutils.core import setup, Extension
 
 cmd = 'gfortran ./glmnet_python/GLMnet.f -fPIC -fdefault-real-8 -shared -o ./glmnet_python/GLMnet.so'
 os.system(cmd)
 
-setup(name='glmnet_python_hanfang',
-	version = '0.1',
-	description = 'Python version of glmnet, originally from Stanford University, modified by Han Fang',
+setup(name='glmnet_python',
+        version = '0.1',
+        description = 'Python version of glmnet, originally from Stanford University, modified by Han Fang',
         url="https://github.com/hanfang/glmnet_python",
-	author = 'Han Fang',
-	author_email = 'hanfang.cshl@gmail.com',
-	license = 'GPL-2',
-	packages=['glmnet_python'],
-	zip_safe = False)
+        author = 'Han Fang',
+        author_email = 'hanfang.cshl@gmail.com',
+        license = 'GPL-2',
+        packages=['glmnet_python'],
+        package_data={'glmnet_python': ['*.so', 'glmnet_python/*.so']},
+        zip_safe = False)
+
