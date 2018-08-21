5c8b1d1b2cfa953e6f7661834559715c6080a79f
Lei Huang <lh389@cornell.edu>
2018-08-21 11:25:28 -0700

    Update glmnetPredict.py
    Fix the scipy.dot typo

--- glmnet_python/glmnetPredict.py
+++ glmnet_python/glmnetPredict.py
@@ -163,7 +163,8 @@ def glmnetPredict(fit,\
             result = nonzeroCoef(nbeta[1:nbeta.shape[0], :], True)
             return(result)
         # use scipy.sparse.hstack instead of column_stack for sparse matrices        
-        result = scipy.dot(scipy.column_stack( (scipy.ones([newx.shape[0], 1]) \
+        result = 
+        (scipy.column_stack( (scipy.ones([newx.shape[0], 1]) \
                               , newx) ) , nbeta)
         if fit['offset']:
             if len(offset) == 0:
@@ -273,7 +274,7 @@ def glmnetPredict(fit,\
             result = nonzeroCoef(nbeta, True)
             return(result)
         
-        result = scipy.dot(newx * nbeta)
+        result = scipy.dot(newx, nbeta)
         
         if fit['offset']:
             if len(offset) == 0:
