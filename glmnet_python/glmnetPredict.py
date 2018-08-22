Fix missing colon after else statement and re-add missing scipy.dot to calculation

glmnet_python/glmnetPredict.py
@@ -163,7 +163,7 @@ def glmnetPredict(fit,\
             result = nonzeroCoef(nbeta[1:nbeta.shape[0], :], True)
             return(result)
         # use scipy.sparse.hstack instead of column_stack for sparse matrices        
-        result = (scipy.column_stack( (scipy.ones([newx.shape[0], 1]) , newx) ) , nbeta)
+        result = scipy.dot(scipy.column_stack( (scipy.ones([newx.shape[0], 1]) , newx) ) , nbeta)
         
         if fit['offset']:
             if len(offset) == 0: