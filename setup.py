82712c6a53cb20f4f5632661ff6bcce89e177d38
John Lees <lees.john6@gmail.com>
2018-09-11 16:26:26 -0400

    Remove pyseer references

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import os, sys
 from setuptools import setup, find_packages
 import subprocess
 
-setup(name='glmnet_python_pyseer',
+setup(name='glmnet_python',
       version = '0.2.2',
       description = 'Python version of glmnet, from Stanford University',
       long_description=open('README.md').read(),
       url="https://github.com/johnlees/glmnet_python",
       author = 'Han Fang (modified by John Lees)',
-      author_email = 'hanfang.cshl@gmail.com (john@johnlees.me)',
+      author_email = 'hanfang.cshl@gmail.com,john@johnlees.me',
       license = 'GPL-2',
       packages=['glmnet_python'],
       install_requires=['joblib>=0.10.3'],
